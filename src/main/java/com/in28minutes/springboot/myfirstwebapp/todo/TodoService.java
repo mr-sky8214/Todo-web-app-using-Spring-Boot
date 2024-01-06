dab9f5c823881b9bb28a0ff533e4f0a9d44a3637
Akash Kamble <kambleakash8214@gmail.com>
2024-01-06 10:26:18 +0530

    Updating and deleting of Todos along with date picker for update

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
@@ -1,17 +1,19 @@
 package com.in28minutes.springboot.myfirstwebapp.todo;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 @Service
 public class TodoService {
     private static List<Todo> todos = new ArrayList<>();
     private static int todosCount = 0;
     static {
-        todos.add(new Todo(++todosCount, "akash", "Learn AWS", LocalDate.now().plusYears(1), false));
+        todos.add(new Todo(++todosCount, "akash", "Get AWS certified", LocalDate.now().plusYears(1), false));
         todos.add(new Todo(++todosCount, "akash", "Learn DevOps", LocalDate.now().plusYears(2), false));
         todos.add(new Todo(++todosCount, "akash", "Learn full stack", LocalDate.now().plusYears(3), false));
     }
@@ -22,5 +24,20 @@ public class TodoService {
 
     public void addTodo(String name, String description, LocalDate targetDate, boolean done) {
         todos.add(new Todo(++todosCount, name, description, targetDate, done));
+    }
+
+    public void deleteById(int id) {
+        Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+        todos.removeIf(predicate);
+    }
+
+    public Todo findById(int id) {
+        Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+        return todos.stream().filter(predicate).findFirst().get();
+    }
+
+    public void updateTodo(@Valid Todo todo) {
+        deleteById(todo.getId());
+        todos.add(todo);
     }
 }
